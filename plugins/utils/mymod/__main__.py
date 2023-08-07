c449a7d5afe060cb4a6f7084c67763f2d828cb76
gudmeong <privatemymail758@gmail.com>
2023-08-08 06:55:19 +0700

    imorove
    Signed-off-by: gudmeong <privatemymail758@gmail.com>

--- plugins/utils/mymod/__main__.py
+++ plugins/utils/mymod/__main__.py
@@ -48,10 +48,20 @@ async def copied(msg: Message):
             await msg.reply_photo(photo=photo, caption=gmsg.caption, quote=1)
             await aiofiles.os.remove(photo)
         else:
-            await msg.reply_text(gmsg.text)
+            if gmsg.video:
+                await msg.reply_video(video=gmsg.video.file_id, caption=gmsg.caption or "", quote=1)
+            elif gmsg.document:
+                await msg.reply_document(document=gmsg.document.file_id, caption=gmsg.caption or "", quote=1)
+            else:
+                await msg.reply_text(gmsg.text if gmsg.text else gmsg.caption or "None")
         await msg.edit("Successfully.")
     else:
-        await msg.reply_text(gmsg.text)
+        if gmsg.video:
+            await msg.reply_video(video=gmsg.video.file_id, caption=gmsg.caption or "", quote=1)
+        elif gmsg.document:
+            await msg.reply_document(document=gmsg.document.file_id, caption=gmsg.caption or "", quote=1)
+        else:
+            await msg.reply_text(gmsg.text if gmsg.text else gmsg.caption or "None")
         await msg.edit("Successfully.")
 
 @pool.run_in_thread
